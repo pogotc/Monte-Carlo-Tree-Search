14964023ecceb33bca85352d6d6a7d61dadcedf3
Steve Ellis <steve.ellis@talis.com>
2020-09-27 11:22:45 +0100

    Some tweaks to the projec to hopefully make it play nicer in npm

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// import MCTS from './MCTS';
+// import MCTSGameState from './MCTSGameState';
+
+export { default as MCTS } from './MCTS';
+export { default as MCTSGameState } from './MCTSGameState';
